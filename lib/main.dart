347c4be9679c87cacbe7c52fc73be6cfa21dc317
nrkhan-233 <redoykhan8141@gmail.com>
2023-03-20 23:12:57 +0600

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:emart/views/splash_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'consts/consts.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appname,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+        fontFamily: regular,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:emart/consts/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: redColor,
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
